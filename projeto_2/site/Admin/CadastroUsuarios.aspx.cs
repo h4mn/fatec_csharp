7b47d6e6d002e7dacb4ac11ffb519fdffc3bb9e4
H. Nunes <670202+h4mn@users.noreply.github.com>
2023-05-10 11:20:03 -0300

    add: adiciona outras alterações

--- projeto_2/site/Admin/CadastroUsuarios.aspx.cs
+++ projeto_2/site/Admin/CadastroUsuarios.aspx.cs
@@ -23,35 +23,27 @@ namespace site.Admin
                 lblMensagem.Text = "O campo Nome é obrigatório!";
                 return;
             } else {
-                // Se os campos estiverem válidos, então cria conexão com o banco de dados
-                // https://www.connectionstrings.com/access
-                // Provider=Microsoft.Jet.OLEDB.12.0;Data Source=C:\mydatabase.mdb;User Id=admin;Password=;
+                try
+                {
+                    // Se os campos estiverem válidos, então cria conexão com o banco de dados
+                    // https://www.connectionstrings.com/access
+                    string conexao = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={HttpContext.Current.Server.MapPath("~/App_Data/Database/Usuarios.accdb")};Persist Security Info=False;";
 
-                string conexao = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + HttpContext.Current.Server.MapPath("~/App_Data/Database/Usuarios.accdb") + ";Persist Security Info=False;";
+                    //<!-- Campos: Nome Completo, Email, NomeAcesso, Anotacoes, Senha -->
+                    string comando = $"INSERT INTO Usuarios (NomeCompleto, Email, NomeAcesso, Anotacoes, Senha) VALUES ('{txtNomeCompleto.Text}', '{txtEmail.Text}', '{txtNomeAcesso.Text}', '{txtAnotacoes.Text}', '{txtSenha.Text}')";
 
-                //<!-- Campos: Nome Completo, Email, NomeAcesso, Anotacoes, Senha -->
-                string comando = $"INSERT INTO Usuarios (NomeCompleto, Email, NomeAcesso, Anotacoes, Senha) VALUES ('{txtNome.Text}', '{txtEmail.Text}', '{txtNomeAcesso.Text}', '{txtAnotacoes.Text}', '{txtSenha.Text}')";
+                    DAO acesso = new DAO();
+                    acesso.DataProviderName = DAO.ProviderName.OleDb;
+                    acesso.ConnectionString = conexao;
+                    acesso.Query(comando);
 
-                // Criar objeto da classe AcessoDados
-                DAO acesso = new DAO(conexao);
-                acesso.DataProviderName = DAO.DataProvider.OleDb;
-                acesso.ConnectionString = conexao;
-                acesso.Query = comando;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
             }
-
-
-            // Enviar o comando SQL para o banco de dados inserir um novo usuário
-
-
-            //Criar objeto da classe Usuario
-
-            //Atribuir os valores dos campos do formulário para as propriedades do objeto
-
-            //Chamar o método Inserir da classe Usuario
-
-            //Limpar os campos do formulário
-
-            //Exibir mensagem de sucesso
             lblMensagem.Text = "Usuário cadastrado com sucesso!";
         }
        
