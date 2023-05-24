d4dd8e2a0e927db6ff6878fe1cff2591781d7c4e
H. Nunes <670202+h4mn@users.noreply.github.com>
2023-05-24 09:28:03 -0300

    add: adiciona metodo Entrar_Click

--- projeto_2/site/Login.aspx.cs
+++ projeto_2/site/Login.aspx.cs
@@ -16,7 +16,28 @@ namespace site
 
         protected void Entrar_Click(object sender, EventArgs e)
         {
-
+            if (NomeAcesso.Text == "Admin" && Senha.Text == "12345")
+            {
+                // Cria a variavel de sessão para identificar que o usuário esta autenticado e
+                // permitir a exibição das opções do menu.
+                Session["autenticado"] = "";
+                // 1. Inicializa a classe de autenticação
+                System.Web.Security.FormsAuthentication.Initialize();
+                // 2. CRIAR O TICKET
+                FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1, "admin",
+                DateTime.Now, DateTime.Now.AddMinutes(20), false,
+                FormsAuthentication.FormsCookiePath);
+                // 3. CRIPTOGRAFA P TICKET E GRAVAR NO COOKIE DO NAVEGADOR
+                Response.Cookies.Add(new HttpCookie(FormsAuthentication.FormsCookieName,
+                FormsAuthentication.Encrypt(ticket)));
+                // Redireciona para o form que o usuário tentou acessar
+                Response.Redirect(FormsAuthentication.GetRedirectUrl("Admin", false));
+            }
+            else
+            {
+                Erro.Text = "Dados de acesso invalidos";
+            }
+            }
         }
     }
 }
